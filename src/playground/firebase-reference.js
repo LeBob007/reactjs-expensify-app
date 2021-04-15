fbf920d6f97395631bf45df33656a28cdacafd1f
Robert Hu <robert.y.hu.97@gmail.com>
2021-04-15 15:13:25 -0700

    Removing env files and changing playground file

--- src/playground/firebase-reference.js
+++ src/playground/firebase-reference.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyC67ncRaWHoojR0SXGkgNCJujp6m2hO5ws",
-    authDomain: "expensify-reactjs-81949.firebaseapp.com",
-    databaseURL: "https://expensify-reactjs-81949-default-rtdb.firebaseio.com",
-    projectId: "expensify-reactjs-81949",
-    storageBucket: "expensify-reactjs-81949.appspot.com",
-    messagingSenderId: "748242792957",
-    appId: "1:748242792957:web:38cbd524b7a7df9b0ee994",
-    measurementId: "G-SDSZ7WJJG1"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
